3b12583eb530889df6b413fbe0aa45175087637d
restas13 <72560360+restas13@users.noreply.github.com>
2023-08-08 12:34:07 -0700

    adding the api routes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const { Post } = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const [affected] = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (affected > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err);
+    }
+});
+
+router.post('/', withAuth, async (req, res) => {
+    const body = req.body;
+
+    try {
+        const newPost = await Post.create({ ...body, userId: req.session.userId });
+        console.log(newPost);
+        res.json(newPost);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/newUser', async (req, res) => {
+  try {
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password,S
+    });
+
+    // Saves the session as logged in
+    req.session.save(() => {
+      req.session.userId = newUser.id;
+      req.session.username = newUser.username;
+      req.session.loggedIn = true;
+
+      res.json(newUser);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// For loggin in
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+
+    if (!user) {
+      res.status(400).json({ message: 'Sorry, the account could not be found' });
+      return;
+    }
+
+    const validPassword = user.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Sorry, it looks like your password was incorrect, please try again' });
+      return;
+    }
+
+    // Saving the session as logged in
+    req.session.save(() => {
+      req.session.userId = user.id;
+      req.session.username = user.username;
+      req.session.loggedIn = true;
+
+      res.json({ user, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json({ message: 'No user account found!' });
+  }
+});
+
+// Logs the user out
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
