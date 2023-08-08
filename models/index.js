c2751d75a3d3637cdc64c846e81886f4e9370d9b
restas13 <72560360+restas13@users.noreply.github.com>
2023-08-08 11:07:46 -0700

    adding the post schema and index.js file

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const User = require('./user');
+const Post = require('./post');
+
+Post.belongsTo(User, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE',
+});
+
+module.exports = { User, Post }
