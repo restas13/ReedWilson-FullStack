6266a347fe09f569817b1f5d36e3d54229a290ef
restas13 <72560360+restas13@users.noreply.github.com>
2023-08-08 16:09:24 -0700

    attempted to debug some issues with the posts

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,21 +1,25 @@
-const newFormHandler = async function(event) {
+const newFormHandler = async function (event) {
     event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-title"]').value;
-    const body = document.querySelector('textarea[name="post-body"]').value;
-  
+
+    const title = document.querySelector('#post-title').value;
+    const body = document.querySelector('#post-body').value;
+
+    console.log(title);
+    console.log(body);
     await fetch(`/api/post`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        body,
-      }),
-      headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        content: JSON.stringify({
+            title,
+            body,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+
     });
-  
+
     document.location.replace('/');
-  };
-  
-  document
+    console.log('ran');
+};
+
+document
     .querySelector('#Search-Button')
-    .addEventListener('submit', newFormHandler);
+    .addEventListener('click', newFormHandler);
