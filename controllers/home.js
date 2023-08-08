9c06367561e7433d34d26a0ad8449fd177d04649
restas13 <72560360+restas13@users.noreply.github.com>
2023-08-08 12:23:12 -0700

    adding the routes for individual posts and logging in

---
+++ controllers/home.js
@@ -0,0 +1,51 @@
+const router = require('express').Rounter();
+const { Post, User } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [User],
+        });
+
+        const posts = PostData.map((post) => {
+            post.get({ plain: true });
+        });
+
+        res.render('allPosts', { posts });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/posts/:id', async (req, res) => {
+    try {
+        const data = await Post.findByPk(req.params.id, {
+            include: User,
+        });
+
+        if (postData) {
+            const post = data.get({ plain: true });
+
+            res.render('individualPost', ( post ));
+        }else {
+            res.status(404).end()
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
+
+router.get('/signin', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+  
+
+  
+  module.exports = router;
+  
